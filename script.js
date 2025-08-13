cb403c39593f6bd11b67a70ab15019921b9f1856
reginaldmj <reginald.jeffersonm@gmail.com>
2025-08-12 20:04:52 -0500

    responsive portfolio

---
+++ script.js
@@ -0,0 +1,5 @@
+// Simple contact form alert
+document.getElementById('contactForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Thank you! Your message has been sent.');
+});
